c9da0ef33baac40a03858222cc33ddd6b2ba6c2d
Mohit Malik <momalik@adobe.com>
2025-01-10 19:49:12 +0530

    Update to check code being rendered from develop branch on different site

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -10,7 +10,7 @@ export default async function decorate(block) {
   const footerMeta = getMetadata('footer');
   const footerPath = footerMeta ? new URL(footerMeta, window.location).pathname : '/footer';
   const fragment = await loadFragment(footerPath);
-  console.log("Footer from the develop branch");
+  console.log('Footer from the develop branch');
   // decorate footer DOM
   block.textContent = '';
   const footer = document.createElement('div');
