8bf2b0a7e1d3faee040dc5eb22a6d9fc9be09dc4
Mohit Malik <momalik@adobe.com>
2025-02-11 12:29:30 +0530

    Update to remove the decoration

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -114,7 +114,7 @@ export function decorateMain(main) {
   buildAutoBlocks(main);
   decorateSections(main);
   decorateBlocks(main);
-  decorateDMImages(main);
+  //decorateDMImages(main);
   console.log('Decorate Main Finished');
 }
 
