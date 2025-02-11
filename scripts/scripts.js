cc4e12a98995e0d59db47ea8d9fe9e4516c87329
Mohit Malik <momalik@adobe.com>
2025-02-11 18:43:34 +0530

    Updates updating to decorete dynamic media images

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -114,6 +114,7 @@ export function decorateMain(main) {
   buildAutoBlocks(main);
   decorateSections(main);
   decorateBlocks(main);
+  decorateDMImages(main);
   console.log('Decorate Main Finished');
 }
 
