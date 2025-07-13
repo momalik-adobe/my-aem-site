3f0d1c17c06e6bc3dfc6d3af9852481e0c95fa4f
Mohit Malik <momalik@adobe.com>
2025-07-13 20:30:50 +0530

    Update

--- blocks/quote/quote.js
+++ blocks/quote/quote.js
@@ -1,5 +1,5 @@
 export default function decorate(block) {
   const [quoteText, auhtor] = block.querySelectorAll('p');
-  block.innerHTML = (`<span> class="quote-text">${quoteText.textContent}</span>
+  block.innerHTML = (`<span class="quote-text">${quoteText.textContent}</span>
     <span class="quote-author">${auhtor.textContent}</span>`);
 }
