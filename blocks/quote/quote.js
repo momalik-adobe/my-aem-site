6ede51116cea28cef42655bd00d0bbef7ff39cfd
Mohit Malik <momalik@adobe.com>
2025-07-13 20:12:37 +0530

    Update

--- blocks/quote/quote.js
+++ blocks/quote/quote.js
@@ -1,14 +1,6 @@
 export default function decorate(block) {
-  const quoteBlock = document.createElement('blockquote');
-  quoteBlock.className = 'quote-block';
-  const quoteText = block.querySelector('.quote-text') || document.createElement('p');
-  quoteText.className = 'quote-text';
-  quoteText.textContent = block.dataset.quote || 'Quote text goes here.';
-  const author = block.querySelector('.quote-author') || document.createElement('cite');
-  author.className = 'quote-author';
-  author.textContent = block.dataset.author || 'Author Name';
-
-  // Append the text and author to the quote block
-  quoteBlock.append(quoteText, author);
-  block.append(quoteBlock);
+  const [p] = block.children;
+  p.array.forEach((element) => {
+    element.classList.add('custom-class');
+  });
 }
